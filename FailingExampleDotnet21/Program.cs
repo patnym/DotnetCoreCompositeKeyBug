b8fd44e96d5c8b26db2e9eb50ea0ef7b48bb820a
Patrik Nyman <patrik.nyman@virtualstores.se>
2018-06-29 19:02:41 +0200

    Updated projects to be a bit more clear of what is working and not working

--- FailingExampleDotnet21/Program.cs
+++ FailingExampleDotnet21/Program.cs
@@ -4,7 +4,25 @@ namespace FailingExampleDotnet21 {
     class Program {
         static void Main(string[] args) {
             using(var context = new MyDbContext()) {
-                var myItem = new MainItem {
+
+                //Works
+                context.MainItems.Add(new MainItem {
+                    CompositeItems = new List<CompositeItem> {
+                        new CompositeItem {
+                            Id = 1,
+                            ItemType = CompositeItem.Type.TypeA
+                        },
+                        new CompositeItem {
+                            Id = 2,
+                            ItemType = CompositeItem.Type.TypeB
+                        }
+                    }
+                });
+
+                System.Console.WriteLine("Successfully added where the composite items use different ItemTypes ");
+
+                //Does not work
+                context.MainItems.Add(new MainItem {
                     CompositeItems = new List<CompositeItem> {
                         new CompositeItem {
                             Id = 3,
@@ -15,9 +33,10 @@ namespace FailingExampleDotnet21 {
                             ItemType = CompositeItem.Type.TypeA
                         }
                     }
-                };
+                });
 
-                context.MainItems.Add(myItem);
+                System.Console.WriteLine("Successfully added where the composite items share ItemType");
+
                 context.SaveChanges();
             }
         }
--- WorkingExampleDotnet20/Program.cs
+++ WorkingExampleDotnet20/Program.cs
@@ -1,10 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WorkingExampleDotnet20 {
     class Program {
         static void Main(string[] args) {
             using(var context = new MyDbContext()) {
-                var myItem = new MainItem {
+
+                //Works
+                context.MainItems.Add(new MainItem {
+                    CompositeItems = new List<CompositeItem> {
+                        new CompositeItem {
+                            Id = 1,
+                            ItemType = CompositeItem.Type.TypeA
+                        },
+                        new CompositeItem {
+                            Id = 2,
+                            ItemType = CompositeItem.Type.TypeB
+                        }
+                    }
+                });
+
+                System.Console.WriteLine("Successfully added where the composite items use different ItemTypes ");
+
+                //Works
+                context.MainItems.Add(new MainItem {
                     CompositeItems = new List<CompositeItem> {
                         new CompositeItem {
                             Id = 3,
@@ -15,9 +35,10 @@ namespace WorkingExampleDotnet20 {
                             ItemType = CompositeItem.Type.TypeA
                         }
                     }
-                };
+                });
 
-                context.MainItems.Add(myItem);
+                System.Console.WriteLine("Successfully added where the composite items share ItemType");
+
                 context.SaveChanges();
             }
         }
